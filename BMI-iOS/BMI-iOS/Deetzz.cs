ac13efccd0370e8f6de4e2edf25c44e30fc117b6
Luigis2 <32309850+Luigis2@users.noreply.github.com>
2017-10-18 13:49:54 -0600

    BET MULT PAGES

---
+++ BMI-iOS/BMI-iOS/Deetzz.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace BMIiOS
+{
+    public partial class Deetzz : UIViewController
+    {
+        public Deetzz (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
---
+++ BMI-iOS/BMI-iOS/bmiControlView.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace BMIiOS
+{
+    public partial class bmiControlView : UIViewController
+    {
+        public bmiControlView (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
